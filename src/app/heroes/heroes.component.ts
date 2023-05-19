ee6fd8a70c141d3edd67c3a88a6e82fef3acf364
dialsuro <doguillo96@gmail.com>
2023-05-19 11:49:39 +0200

    added services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 @Component({
   selector: 'app-heroes',
@@ -8,9 +8,17 @@ import { Hero } from '../hero';
   
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-}
+  }
+  ngOnInit(): void {
+  this.getHeroes();
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+  constructor(private heroService: HeroService) {}
 }
